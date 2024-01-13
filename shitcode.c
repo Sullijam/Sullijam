6bce5808420c337809e1ea3af5eecee742072eeb
Sullijam <isilow02@gmail.com>
2024-01-13 19:01:22 +0100

    Made funny code

---
+++ shitcode.c
@@ -0,0 +1,40 @@
+#include "stdio.h"
+#include "stdbool.h"
+#include "math.h"
+
+typedef unsigned int yummy;
+typedef struct banana banan;
+typedef int hahaha;
+
+// fucking sick struct
+struct banana {
+    yummy long_; // TODO: make it yummier
+    long dont_touch;}; 
+
+/// @brief Bestest funktion äver
+/// @param apple an apple (duh?)
+/// @return idk ¯\_(ツ)_/¯
+/// @note "Stroke har inte tur i ordet"
+yummy funny_function_hehe(banan apple) // :)))
+{
+    hahaha vIC = sqrt((hahaha)(50 - 50) + (0 * 12345) + (0 / 1 + 1 - 1)+(sizeof(int) - 1) * 3)-3+(!      (1==1))+(0&&      123)+(0||0)-(0>1);
+    yummy veryIncorrectName = apple.long_; // java mömänt
+    hahaha idk;
+    for(veryIncorrectName;veryIncorrectName<apple.dont_touch;veryIncorrectName++)
+    { // BAAAAAR IIIIII HUUUUUUUUUUUUS
+/*Kul med kommentarer*/        if(vIC%veryIncorrectName>=1){if(veryIncorrectName==false){break;}else{vIC=sizeof(unsigned long)-8;}
+        }else{ /*En till här. Måste ju va tydlig med vad som händer*/ 
+            if(2==1||veryIncorrectName>12){veryIncorrectName++;idk=veryIncorrectName;idk%=apple.dont_touch;
+            }else{continue;}}} 
+    if (apple.long_ == sizeof(size_t) + sizeof(long)){funny_function_hehe(apple);}else{veryIncorrectName = vIC;}
+    return idk; // THEY DONT KNOW ME SON
+}
+
+int main(int argc, char const *argv[])
+{
+    struct banana orange;
+    orange.long_ = 2;
+    orange.dont_touch = 101;
+    printf("%d\n", funny_function_hehe(orange));
+    return 0;
+}
